6e8cefa1a8ff1f215864875e5e46a8731da92816
Jonghyun <zxc159285@gmail.com>
2026-07-06 09:46:44 +0900

    app

--- healthcareFront/src/App.jsx
+++ healthcareFront/src/App.jsx
@@ -6,8 +6,10 @@ import MemberMain from './MemberMain.jsx';
 import './App.css';
 import B2cMain from './b2c_mypage/B2cMain.jsx';
 import B2cComplaint from './b2c_mypage/B2cComplaint.jsx';
-//import Itempage from './item/Itempage.jsx';
-//import Userpage from './user/Userpage.jsx';
+import ContractNew from './user/ContractNew.jsx';
+import Itempage from './item/Itempage.jsx';
+import Userpage from './user/Userpage.jsx';
+
 
 // 메인 애플리케이션 컴포넌트
 function App() {
@@ -17,11 +19,12 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/join" element={<Join />} />
       <Route path="/fitc" element={<MemberMain />} />
-      <Route path="/fitc/mypage" element={<B2cMain />}/>
+      <Route path="/fitc/mypage" element={<B2cMain />} />
       <Route path="/fitc/mypage/b2ccomplaint" element={<B2cComplaint />} />
       <Route path="/fitb" element={<AdminMain />} />
-      {/* <Route path="/fitb/itempage" element={<Itempage />} />
-      <Route path="/fitb/userpage" element={<Userpage />} /> */}
+      <Route path="/fitb/contract/new" element={<ContractNew />} />
+      <Route path="/fitb/itempage" element={<Itempage />} />
+      <Route path="/fitb/userpage" element={<Userpage />} />
     </Routes>
   );
 }
