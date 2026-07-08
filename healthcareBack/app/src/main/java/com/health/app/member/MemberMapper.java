706b3d3741aca6f9146c559ee4b4997ff172fbb5
Jonghyun <zxc159285@gmail.com>
2026-07-08 11:05:51 +0900

    dasda

--- healthcareBack/app/src/main/java/com/health/app/member/MemberMapper.java
+++ healthcareBack/app/src/main/java/com/health/app/member/MemberMapper.java
@@ -10,4 +10,7 @@ public interface MemberMapper {
 
     // 아이디 중복확인 메서드
     public MemberDTO idcheck(MemberDTO memberDTO) throws Exception;
+
+    // 회원정보 수정 메서드
+    public int update(MemberDTO memberDTO) throws Exception;
 }
